d4ec1b660e81881252e6de652b6b19ab43092508
unknown <251684246@formanite.fccollege.edu.pk>
2025-05-12 20:54:42 +0500

    [Video#3] complete apple tree

--- survival_2d/script/apple_tree.gd
+++ survival_2d/script/apple_tree.gd
@@ -3,6 +3,8 @@ extends Node2D
 var state = "no apples"  # no apples, apples
 var player_in_area = false
 
+var apple = preload("res://scene/apple_collectable.tscn")
+
 func _ready():
 	if state == "no apples":
 		$growth_timer.start()
@@ -15,7 +17,7 @@ func _process(delta):
 		if player_in_area:
 			if Input.is_action_just_pressed("e"):
 				state = "no apples"
-				$growth_timer.start()
+				drop_apple()
 
 
 func _on_pickable_area_body_entered(body):
@@ -30,3 +32,12 @@ func _on_pickable_area_body_exited(body: Node2D) -> void:
 func _on_growth_timer_timeout():
 	if state == "no apples":
 		state = "apples"
+
+func drop_apple():
+	var apple_instance = apple.instantiate()
+	apple_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(apple_instance)
+	
+	await get_tree().create_timer(3).timeout
+	$growth_timer.start()
+	
