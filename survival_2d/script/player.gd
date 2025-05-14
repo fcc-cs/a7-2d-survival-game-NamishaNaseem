545044fa2c2565014d3e5745a49e0c19f9d496fc
unknown <251684246@formanite.fccollege.edu.pk>
2025-05-14 15:13:28 +0500

    [Video#6] arrow movement without graphic

--- survival_2d/script/player.gd
+++ survival_2d/script/player.gd
@@ -6,6 +6,10 @@ var player_state
 
 @export var inv: Inv
 
+var bow_equipped = true
+var bow_cooldown = true
+var arrow = preload("res://scene/arrow.tscn")
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	
@@ -16,6 +20,19 @@ func _physics_process(delta):
 	
 	velocity = direction * speed
 	move_and_slide()
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("left_mouse") and bow_equipped and bow_cooldown:
+		bow_cooldown = false
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+		
+		await get_tree().create_timer(0.4).timeout
+		bow_cooldown = true
 	
 	play_anim(direction)
 	
