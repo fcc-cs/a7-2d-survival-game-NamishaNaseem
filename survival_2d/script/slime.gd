701a22fa827446b52d175343a8b497f23270c8d6
unknown <251684246@formanite.fccollege.edu.pk>
2025-05-16 15:10:03 +0500

    [Video#8] enemy collectable done, completed

--- survival_2d/script/slime.gd
+++ survival_2d/script/slime.gd
@@ -8,6 +8,9 @@ var dead = false
 var player_in_area = false
 var player
 
+@onready var slime = $slime_collectable
+@export var itemRes: InvItem
+
 func _ready():
 	dead = false
 
@@ -50,4 +53,24 @@ func death():
 	dead = true
 	$AnimatedSprite2D.play("death")
 	await get_tree().create_timer(1).timeout
+	drop_slime()
+	$AnimatedSprite2D.visible = false
+	$hitbox/CollisionShape2D.disabled = true
+	$detection_area/CollisionShape2D.disabled = true
+
+
+func drop_slime():
+	slime.visible = true
+	$slime_collect_area/CollisionShape2D.disabled = false
+	slime_collect()
+
+func slime_collect():
+	await get_tree().create_timer(1.5).timeout
+	slime.visible = false
+	player.collect(itemRes)
 	queue_free()
+
+
+func _on_slime_collect_area_body_entered(body):
+	if body.has_method("player"):
+		player = body
