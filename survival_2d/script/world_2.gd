f09e398f741a3aa4e03097b6a56eb0002aeb0517
unknown <251684246@formanite.fccollege.edu.pk>
2025-05-16 21:37:50 +0500

    [Video#9] smoke and camera done, completed

--- survival_2d/script/world_2.gd
+++ survival_2d/script/world_2.gd
@@ -1,13 +1,39 @@
 extends Node2D
 
 @onready var animplayer = $world2openingcutscene/AnimationPlayer
+@onready var camera = $world2openingcutscene/Path2D/PathFollow2D/Camera2D
 
 var is_openingcutscene = false
 
 var has_player_entered_area = false
 var player = null
 
+var is_pathfollowing = false
 
+var smoke_has_happened = false
+var smoke_is_happening = false
+
+func _physics_process(delta):
+	if is_openingcutscene:
+		var pathfollower = $world2openingcutscene/Path2D/PathFollow2D
+		
+		if is_pathfollowing:
+			if !smoke_is_happening:
+				pathfollower.progress_ratio += 0.001
+			
+			if pathfollower.progress_ratio >= 0.98:
+				cutsceneending()
+			
+			if !smoke_has_happened and pathfollower.progress_ratio >= 0.78 and !smoke_is_happening:
+				smoke_is_happening = true
+				toggle_smoke()
+				await get_tree().create_timer(1).timeout
+				$world2openingcutscene/TileMapFinished.visible = true
+				$world2openingcutscene/TileMapUnfinished.visible = false
+				toggle_smoke()
+				await get_tree().create_timer(0.5).timeout
+				smoke_has_happened = true
+				smoke_is_happening = false
 
 func _on_player_detection_body_entered(body):
 	if body.has_method("player"):
@@ -20,4 +46,23 @@ func cutsceneopening():
 	is_openingcutscene = true
 	animplayer.play("cover_fade")
 	player.camera.enabled = false
-	#enable our own camera
+	camera.enabled = true
+	is_pathfollowing = true
+
+func cutsceneending():
+	is_pathfollowing = false
+	is_openingcutscene = false
+	camera.enabled = false
+	player.camera.enabled = true
+	$world2openingcutscene.visible = false
+	$world2main.visible = true
+
+func toggle_smoke():
+	var smoke1 = $world2openingcutscene/SmokeParticles1
+	var smoke2 = $world2openingcutscene/SmokeParticles2
+	var smoke3 = $world2openingcutscene/SmokeParticles3
+	var smoke4 = $world2openingcutscene/SmokeParticles4
+	smoke1.emitting = !smoke1.emitting
+	smoke2.emitting = !smoke2.emitting
+	smoke3.emitting = !smoke3.emitting
+	smoke4.emitting = !smoke4.emitting
